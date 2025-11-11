9bdcb2a700abee94e896db03a460a6d101816a17
Vitaliy Zlenko <Vitaliy.Zlenko@miratechgroup.com>
2025-11-11 19:38:45 +0100

    Added mission and rocket statuses

---
+++ src/main/java/com/example/spacex/model/MissionStatus.java
@@ -0,0 +1,8 @@
+package com.example.spacex.model;
+
+public enum MissionStatus {
+    SCHEDULED,
+    PENDING,
+    IN_PROGRESS,
+    ENDED
+}
---
+++ src/main/java/com/example/spacex/model/RocketStatus.java
@@ -0,0 +1,7 @@
+package com.example.spacex.model;
+
+public enum RocketStatus {
+    ON_GROUND,
+    IN_SPACE,
+    IN_REPAIR
+}
